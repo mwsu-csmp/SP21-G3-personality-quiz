ec652fdf6b845fe1b25e02dc1167a7d0d88bd060
zamattsu <mattsonzavier@gmail.com>
2021-04-10 18:02:25 -0500

    Simple GUI elements added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,74 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Group root = new Group();
+        StackPane rootPane = new StackPane();
+        GridPane grid = new GridPane();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Question question1 = new Question("What is your favorite flavor of ice-cream????", "Chocolate Strawberry Vanilla Rocky_Road", "icecream.gif");
+        System.out.println(question1.toString());
+        Label label = new Label(question1.getQuestion());
+        label.setFont(Font.font("Comic Sans MS", 24));
+        Image image = new Image(question1.getImageDir());
+        grid.add(label, 1, 1, 5, 1);
+        grid.add(new ImageView(image), 1, 2, 5, 1);
+        grid.setGridLinesVisible(true);
+        TilePane buttonGrid = generateButtonLayout(4,question1.getAnswers());
+        buttonGrid.setPadding(new Insets(0, 0, 30, 0));
+        buttonGrid.setAlignment(Pos.BOTTOM_CENTER);
+
+        grid.setAlignment(Pos.TOP_CENTER);
+        rootPane.getChildren().addAll(grid, buttonGrid);
+        primaryStage.setScene(new Scene(rootPane, 600, 700));
+
         primaryStage.show();
+    }
+
+    private TilePane generateButtonLayout(int buttonNum, ArrayList<String> answers){
+        TilePane layout = new TilePane();
+        layout.setHgap(10);
+        layout.setPrefColumns(buttonNum);
+        layout.getChildren().setAll(createButtons(buttonNum, answers));
+        layout.setPadding(new Insets(10));
+
+        return layout;
+    }
+
+    private Button[] createButtons(int buttonNum, ArrayList<String> answers) {
+        Button[] buttons = new Button[buttonNum];
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = createButton(answers.get(i));
+        }
+
+        return buttons;
+    }
+
+    private Button createButton(String answer) {
+        Button button = new Button();
+        button.setText(answer);
+        button.setPrefWidth(125);
+
+        return button;
     }
 
 
--- src/Question.java
+++ src/Question.java
@@ -1,19 +1,45 @@
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+
 public class Question {
     String message;
-    String[] answers;
-    String[] images;
-    public Question(String message, String[] answers, String[] images){
+    ArrayList<String> answers;
+    String image;
+    public Question(String message, String answers, String image){
         this.message = message;
-        this.answers = answers;
-        this.images = images;
+        this.image = image;
+        ArrayList<String> answerList = new ArrayList<String>();
+        StringBuilder current = new StringBuilder();
+        for (int i = 0; i < answers.length(); i++){
+            char c = answers.charAt(i);
+            if(c != ' '){
+                current.append(c);
+            }else {
+                answerList.add(current.toString());
+                current.setLength(0);
+            }
+        }
+        answerList.add(current.toString());
+        this.answers = answerList;
     }
 
     public String toString(){
-        StringBuilder answerString = new StringBuilder();
-        for(int i = 0; i < this.answers.length; i++){
-            answerString.append(i + ", ");
+
+        StringBuffer stringList = new StringBuffer();
+        for(String s : this.answers){
+            stringList.append(s);
         }
-        return "Question = " + message + "| Answers =  " + answerString;
+        return "Question: "+ this.message + "Answers = "+ stringList.toString();
+    }
+    public String getImageDir(){
+        return this.image;
+    }
+    public String getQuestion(){
+        return this.message;
+    }
+    public ArrayList<String> getAnswers(){
+        return this.answers;
     }
 
 }
