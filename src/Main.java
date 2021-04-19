31e417f6bff5041a5ee0fb4031454530eee5016d
zamattsu <mattsonzavier@gmail.com>
2021-04-19 17:46:55 -0500

    Eventhandling mostly done, now on to storing answers and adding more questions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -22,7 +23,9 @@ import java.util.function.Consumer;
 
 
 public class Main extends Application {
-
+    /*
+        Look in GGClient login pane for ideas on how to use consumers and lambdas.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         BorderPane border = new BorderPane();
@@ -38,7 +41,8 @@ public class Main extends Application {
                 "chocolate.gif strawberry.gif vanilla.gif rockyroad.jpg");
         loadQuestion(question1, border);
 
-        primaryStage.setScene(new Scene(border, 600, 700));
+        primaryStage.setScene(new Scene(border, 724, 700));
+        primaryStage.setResizable(false);
         Button sizereturn = new Button("Return size");
         sizereturn.addEventFilter(MouseEvent.MOUSE_CLICKED, miscEvent);
         border.setBottom(sizereturn);
@@ -52,15 +56,21 @@ public class Main extends Application {
         label.setWrapText(true);
         border.setTop(label);
         border.setAlignment(border.getTop(), Pos.CENTER);
-        border.setCenter(new TilePane());
-        TilePane buttons = (TilePane)border.getCenter();
-        Button b1 = new Button(question.getAnswers().get(0));
-        Button b2 = new Button(question.getAnswers().get(1));
-        Button b3 = new Button(question.getAnswers().get(2));
-        Button b4 = new Button(question.getAnswers().get(3));
-        buttons.getChildren().addAll(b1, b2, b3, b4);
+        border.setCenter(new GridPane());
+        GridPane buttons = (GridPane)border.getCenter();
+        Button b1 = new Button();
+        Button b2 = new Button();
+        Button b3 = new Button();
+        Button b4 = new Button();
+        buttons.add(b1, 0, 0, 1, 1);
+        buttons.add(new Label(question.getAnswers().get(0)), 0, 1, 1, 1);
+        buttons.add(b2, 1, 0, 1, 1);
+        buttons.add(new Label(question.getAnswers().get(1)), 1, 1, 1,  1);
+        buttons.add(b3, 0, 2, 1, 1);
+        buttons.add(new Label(question.getAnswers().get(2)), 0, 3, 1, 1);
+        buttons.add(b4, 1, 2, 1, 1);
+        buttons.add(new Label(question.getAnswers().get(3)), 1, 3, 1, 1);
         buttons.setAlignment(Pos.CENTER);
-        buttons.setPrefColumns(2);
         buttons.setHgap(10);
         buttons.setVgap(10);
         ArrayList<ImageView> pictures = new ArrayList<ImageView>();
@@ -68,23 +78,50 @@ public class Main extends Application {
         pictures.add(new ImageView(question.getImageDir().get(1)));
         pictures.add(new ImageView(question.getImageDir().get(2)));
         pictures.add(new ImageView(question.getImageDir().get(3)));
-        for(int i = 0; i < 4; i++){
-            Button currentButton = (Button)buttons.getChildren().get(i);
-            currentButton.setGraphic(pictures.get(i));
-            ButtonHandleClass buttonHandle = new ButtonHandleClass();
-            currentButton.setOnAction(buttonHandle);
+        for(int i = 0; i < 8; i++){
+            if(buttons.getChildren().get(i) instanceof Button) {
+                Button currentButton = (Button)buttons.getChildren().get(i);
+                currentButton.setGraphic(pictures.get(i/2));
+            }
         }
+
         buttons.setPadding(new Insets(10, 50, 10, 50));
-
+        b1.setOnAction(event -> {
+            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b1)+1, buttons.getColumnIndex(b1), buttons);
+            String finalAnswer = answerLabel.getText();
+            System.out.println(finalAnswer);
+        });
+        b2.setOnAction(event -> {
+            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b2)+1, buttons.getColumnIndex(b2), buttons);
+            String finalAnswer = answerLabel.getText();
+            System.out.println(finalAnswer);
+        });
+        b3.setOnAction(event -> {
+            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b3)+1, buttons.getColumnIndex(b3), buttons);
+            String finalAnswer = answerLabel.getText();
+            System.out.println(finalAnswer);
+        });
+        b4.setOnAction(event -> {
+            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b4)+1, buttons.getColumnIndex(b4), buttons);
+            String finalAnswer = answerLabel.getText();
+            System.out.println(finalAnswer);
+        });
     }
 
-    class ButtonHandleClass implements EventHandler<ActionEvent>{
-        @Override
-        public void handle(ActionEvent e){
-            Button sourceButton = (Button)e.getSource();
-            System.out.println(sourceButton.getText());
+    public Node getNodeRowCol(int row, int column, GridPane grid){
+        Node result = null;
+        ObservableList<Node> children = grid.getChildren();
+
+        for (Node node : children) {
+            if(grid.getRowIndex(node) == row && grid.getColumnIndex(node) == column) {
+                result = node;
+                break;
+            }
         }
+
+        return result;
     }
+
 
     public static void main(String[] args) {
         launch(args);
