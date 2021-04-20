8aba6205a4538f6c55c29d79c51da114cc7dd419
zamattsu <mattsonzavier@gmail.com>
2021-04-19 19:59:57 -0500

    Cleaned up main method and added weights to each answer, progressing the user towards a respective outcome

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -29,98 +30,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         BorderPane border = new BorderPane();
-        primaryStage.setTitle("Hello World");
-        EventHandler<MouseEvent> miscEvent = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e){
-                System.out.println("" + border.getWidth() + "||" + border.getHeight());
-            }
-        };
-        Question question1 = new Question("What is your favorite flavor of ice-cream????",
+        primaryStage.setTitle("What Spring Activity are You?");
+        Question question1 = new Question(
+                "What is your favorite flavor of ice-cream????",
                 "Chocolate Strawberry Vanilla Rocky_Road",
+                "<20 10 25 50 5> <5 20 15 15 50> <20 30 20 20 30> <40 15 50 10 20>",
                 "chocolate.gif strawberry.gif vanilla.gif rockyroad.jpg");
-        loadQuestion(question1, border);
+        question1.load(border);
+//TODO: Make the program check if there is a currentAnswer for the last question, if there isn't, then prevent moving to next question
 
-        primaryStage.setScene(new Scene(border, 724, 700));
-        primaryStage.setResizable(false);
-        Button sizereturn = new Button("Return size");
-        sizereturn.addEventFilter(MouseEvent.MOUSE_CLICKED, miscEvent);
-        border.setBottom(sizereturn);
+        border.setPadding(new Insets(25));
+        primaryStage.setScene(new Scene(border, 750, 700));
+
         primaryStage.show();
     }
 
-    private void loadQuestion(Question question, BorderPane border){
-        border.getChildren().clear();
-        Label label = new Label(question.getQuestion());
-        label.setFont(Font.font("Tahoma", 30));
-        label.setWrapText(true);
-        border.setTop(label);
-        border.setAlignment(border.getTop(), Pos.CENTER);
-        border.setCenter(new GridPane());
-        GridPane buttons = (GridPane)border.getCenter();
-        Button b1 = new Button();
-        Button b2 = new Button();
-        Button b3 = new Button();
-        Button b4 = new Button();
-        buttons.add(b1, 0, 0, 1, 1);
-        buttons.add(new Label(question.getAnswers().get(0)), 0, 1, 1, 1);
-        buttons.add(b2, 1, 0, 1, 1);
-        buttons.add(new Label(question.getAnswers().get(1)), 1, 1, 1,  1);
-        buttons.add(b3, 0, 2, 1, 1);
-        buttons.add(new Label(question.getAnswers().get(2)), 0, 3, 1, 1);
-        buttons.add(b4, 1, 2, 1, 1);
-        buttons.add(new Label(question.getAnswers().get(3)), 1, 3, 1, 1);
-        buttons.setAlignment(Pos.CENTER);
-        buttons.setHgap(10);
-        buttons.setVgap(10);
-        ArrayList<ImageView> pictures = new ArrayList<ImageView>();
-        pictures.add(new ImageView(question.getImageDir().get(0)));
-        pictures.add(new ImageView(question.getImageDir().get(1)));
-        pictures.add(new ImageView(question.getImageDir().get(2)));
-        pictures.add(new ImageView(question.getImageDir().get(3)));
-        for(int i = 0; i < 8; i++){
-            if(buttons.getChildren().get(i) instanceof Button) {
-                Button currentButton = (Button)buttons.getChildren().get(i);
-                currentButton.setGraphic(pictures.get(i/2));
-            }
-        }
-
-        buttons.setPadding(new Insets(10, 50, 10, 50));
-        b1.setOnAction(event -> {
-            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b1)+1, buttons.getColumnIndex(b1), buttons);
-            String finalAnswer = answerLabel.getText();
-            System.out.println(finalAnswer);
-        });
-        b2.setOnAction(event -> {
-            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b2)+1, buttons.getColumnIndex(b2), buttons);
-            String finalAnswer = answerLabel.getText();
-            System.out.println(finalAnswer);
-        });
-        b3.setOnAction(event -> {
-            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b3)+1, buttons.getColumnIndex(b3), buttons);
-            String finalAnswer = answerLabel.getText();
-            System.out.println(finalAnswer);
-        });
-        b4.setOnAction(event -> {
-            Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(b4)+1, buttons.getColumnIndex(b4), buttons);
-            String finalAnswer = answerLabel.getText();
-            System.out.println(finalAnswer);
-        });
-    }
-
-    public Node getNodeRowCol(int row, int column, GridPane grid){
-        Node result = null;
-        ObservableList<Node> children = grid.getChildren();
-
-        for (Node node : children) {
-            if(grid.getRowIndex(node) == row && grid.getColumnIndex(node) == column) {
-                result = node;
-                break;
-            }
-        }
-
-        return result;
-    }
 
 
     public static void main(String[] args) {
--- src/Question.java
+++ src/Question.java
@@ -1,41 +1,34 @@
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Question {
-    String message;
-    ArrayList<String> answers;
-    ArrayList<String> images;
+    private String message;
+    private ArrayList<String> answers;
+    private ArrayList<String> images;
+    private String currentAnswer;
+    private ArrayList<List<Integer>> answerWeights;
+//TODO: Make a method that connects current Answer to the correct list of weights from answerWeights
 
-    public Question(String message, String answers, String images){
+    public Question(String message, String answers, String weights, String images){
         this.message = message;
-        ArrayList<String> answerList = new ArrayList<String>();
-        StringBuilder current = new StringBuilder();
-        for (int i = 0; i < answers.length(); i++){
-            char c = answers.charAt(i);
-            if(c != ' '){
-                current.append(c);
-            }else {
-                answerList.add(current.toString());
-                current.setLength(0);
-            }
-        }
-        answerList.add(current.toString());
-
-        current.setLength(0);
-        ArrayList<String> imageList = new ArrayList<String>();
-        for (int i = 0; i < images.length(); i++){
-            char c = images.charAt(i);
-            if(c != ' '){
-                current.append(c);
-            }else {
-                imageList.add(current.toString());
-                current.setLength(0);
-            }
-        }
-        imageList.add(current.toString());
-        this.answers = answerList;
-        this.images = imageList;
+        this.answers = deserialize(answers);
+        this.images = deserialize(images);
+        this.answerWeights = deserializeWeights(weights);
     }
 
     public String toString(){
@@ -52,8 +45,111 @@ public class Question {
     public String getQuestion(){
         return this.message;
     }
-    public ArrayList<String> getAnswers(){
-        return this.answers;
+    public ArrayList<String> getAnswers(){ return this.answers;
     }
 
+    public void load(BorderPane border){
+        border.getChildren().clear();
+        Label label = new Label(this.getQuestion());
+        label.setFont(Font.font("Tahoma", FontWeight.BOLD, 30));
+        label.setStyle("-fx-text-alignment: center");
+        label.setWrapText(true);
+        border.setTop(label);
+        border.setAlignment(border.getTop(), Pos.CENTER);
+        border.setCenter(new GridPane());
+        GridPane buttons = (GridPane)border.getCenter();
+        Button b1 = new Button();
+        Button b2 = new Button();
+        Button b3 = new Button();
+        Button b4 = new Button();
+        buttons.add(b1, 0, 0, 1, 1);
+        buttons.add(new Label(this.getAnswers().get(0)), 0, 1, 1, 1);
+        buttons.add(b2, 1, 0, 1, 1);
+        buttons.add(new Label(this.getAnswers().get(1)), 1, 1, 1,  1);
+        buttons.add(b3, 0, 2, 1, 1);
+        buttons.add(new Label(this.getAnswers().get(2)), 0, 3, 1, 1);
+        buttons.add(b4, 1, 2, 1, 1);
+        buttons.add(new Label(this.getAnswers().get(3)), 1, 3, 1, 1);
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setHgap(10);
+        buttons.setVgap(10);
+        ArrayList<ImageView> pictures = new ArrayList<ImageView>();
+        pictures.add(new ImageView(this.getImageDir().get(0)));
+        pictures.add(new ImageView(this.getImageDir().get(1)));
+        pictures.add(new ImageView(this.getImageDir().get(2)));
+        pictures.add(new ImageView(this.getImageDir().get(3)));
+        for(int i = 0; i < 8; i++){
+            if(buttons.getChildren().get(i) instanceof Button) {
+                Button currentButton = (Button)buttons.getChildren().get(i);
+                currentButton.setGraphic(pictures.get(i/2));
+            }
+        }
+
+        buttons.setPadding(new Insets(10, 50, 10, 50));
+        class ButtonHandler implements EventHandler<ActionEvent> {
+            @Override
+            public void handle(ActionEvent e){
+                Button currentButton = (Button)e.getSource();
+                Label answerLabel = (Label)getNodeRowCol(buttons.getRowIndex(currentButton)+1, buttons.getColumnIndex(currentButton), buttons);
+                String finalAnswer = answerLabel.getText();
+                currentAnswer = finalAnswer;
+                System.out.println(currentAnswer);
+                System.out.println(answerWeights);
+            }
+        }
+        b1.setOnAction(new ButtonHandler());
+        b2.setOnAction(new ButtonHandler());
+        b3.setOnAction(new ButtonHandler());
+        b4.setOnAction(new ButtonHandler());
+    }
+
+    private Node getNodeRowCol(int row, int column, GridPane grid){
+        Node result = null;
+        ObservableList<Node> children = grid.getChildren();
+
+        for (Node node : children) {
+            if(grid.getRowIndex(node) == row && grid.getColumnIndex(node) == column) {
+                result = node;
+                break;
+            }
+        }
+
+        return result;
+    }
+    public String getCurrentAnswer() { return this.currentAnswer; }
+
+    private ArrayList<String> deserialize(String input){
+        ArrayList<String> accList = new ArrayList<String>();
+        StringBuilder current = new StringBuilder();
+        for (int i = 0; i < input.length(); i++){
+            char c = input.charAt(i);
+            if(c != ' '){
+                current.append(c);
+            }else {
+                accList.add(current.toString());
+                current.setLength(0);
+            }
+        }
+        accList.add(current.toString());
+        return accList;
+    }
+
+    private ArrayList<List<Integer>> deserializeWeights(String input){
+        ArrayList<List<Integer>> accList = new ArrayList<List<Integer>>();
+        StringBuilder current = new StringBuilder();
+        int listCount = 0;
+        for (int i = 0; i < input.length(); i++){
+            char c = input.charAt(i);
+            if(c == '<'){
+                ArrayList<Integer> currentList = new ArrayList<Integer>();
+                listCount++;
+                String sub = input.substring(i+1, input.indexOf('>', i));
+                for(String s : deserialize(sub)) currentList.add(Integer.valueOf(s));
+                accList.add(currentList);
+                i = input.indexOf('>', i);
+                continue;
+            }
+        }
+        return accList;
+    }
 }
